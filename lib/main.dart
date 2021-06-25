29d6f4b6e5251338ac5f3d3a8612878c67304a9e
Marcelo Miyachi <miyachimarcelo@gmail.com>
2021-06-25 20:59:15 -0300

    feat: add loginPage and LoginButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/splash_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primaryColor: AppColors.primary),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
